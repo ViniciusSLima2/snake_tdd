4d53e039336d987e04101eb756afb3ffbf22454c
vslima <vinicius.slima021@gmail.com>
2025-10-08 11:05:02 -0300

    Etapa 3: refac in testes

--- testes.py
+++ testes.py
@@ -1,16 +1,17 @@
 import pytest
 from snake import SnakeGame
 
+# Em testes.py da Etapa 3 (CORRIGIDO)
 def test_snake_wraps_around_top_edge():
     game = SnakeGame(10, 10)
-    game.snake_body = [(0, 5)] # No topo
-    game.direction = 'w'
+    game.snake_body = [(0, 5)]
+    game.change_direction('w') # <-- Use o método correto
     game.update()
-    assert game.snake_body[0] == (9, 5) # Deve aparecer na base
+    assert game.snake_body[0] == (9, 5)
 
 def test_snake_wraps_around_right_edge():
     game = SnakeGame(10, 10)
-    game.snake_body = [(5, 9)] # Na direita
-    game.direction = 'd'
+    game.snake_body = [(5, 9)]
+    game.change_direction('d') # <-- Use o método correto
     game.update()
-    assert game.snake_body[0] == (5, 0) # Deve aparecer na esquerda
+    assert game.snake_body[0] == (5, 0)
